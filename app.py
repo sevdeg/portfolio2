eef282d041953593d5c1c9fde5f94a12d8ff205b
sevdeg <s341845@oslomet.no>
2021-04-27 14:50:51 +0200

    s

--- app.py
+++ app.py
@@ -11,5 +11,21 @@ def kontaktOss():
     return render_template('kontaktOss.html')
     #return ("hello world")
 
+@app.route('/checkout')
+def checkout():
+    return render_template('checkout.html')
+    #return ("hello world")
+
+@app.route('/shoppingcart')
+def shoppingcart():
+    return render_template('handlekurv.html')
+    #return ("hello world")
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
+    #return ("hello world")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
