107760734dfdb9ed4463877d11bd3aec3e41500c
sevdeg <s341845@oslomet.no>
2021-05-06 01:32:35 +0200

    fiksa

--- app.py
+++ app.py
@@ -1,5 +1,50 @@
 from flask import Flask, render_template
+import mysql.connector
+import time
+
+time.sleep(30)
+
+db=mysql.connector.connect(
+    host="database",
+    user="root",
+    passwd="admin",
+    database="testdatabase"
+)
+
+mycursor = db.cursor()
+
+try:
+    mycursor.execute("CREATE TABLE Person1 (name VARCHAR(50), age smallint UNSIGNED, personID int PRIMARY KEY AUTO_INCREMENT)")
+except mysql.connector.Error as err:
+    print("Something went wrong: {}".format(err))
+
+db.commit()
+'''
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="2000Oguz",
+    database="webshop_database"
+    )
+
+mycursor = db.cursor()
+#mycursor.execute("CREATE TABLE Person(name VARCHAR(50), age smallint UNSIGNED, personID int PRIMARY KEY AUTO_INCREMENT)")
+
+#mycursor.execute("INSERT INTO Person (name,age) VALUES(%s,%s)",("Tim",19))
+#db.commit()
+mycursor.execute("SELECT * FROM Person")
+myresult=mycursor.fetchall()
+
+for x in mycursor:
+    print(x)
+
+'''
+#ADD DATABASE
+
+#THE API
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -29,4 +74,5 @@ def login():
     #return ("hello world")
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
+    #db.close()
